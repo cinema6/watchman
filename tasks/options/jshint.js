ea68450027b84faaae7b23c1ef5b230dcc5b23da
Scott Bouloutian <ScottBouloutian@gmail.com>
2016-01-19 14:59:07 -0500

    module to produce json into a given stream files and grunt tasks making up the project ecosystem

---
+++ tasks/options/jshint.js
@@ -0,0 +1,11 @@
+module.exports = {
+    options: {
+        jshintrc: true
+    },
+    all: [
+        'src/**/*.js',
+        'tests/**/*.js',
+        'Gruntfile.js',
+        'scripts/**/*.js'
+    ]
+};
