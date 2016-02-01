3814595986ef36a8f9d901f50c4ea98bf11ac334
Scott Bouloutian <ScottBouloutian@gmail.com>
2016-02-01 16:09:44 -0500

    added tdd task

--- tasks/options/watch.js
+++ tasks/options/watch.js
@@ -2,5 +2,14 @@ module.exports = {
     vagrant: {
         files: ['src/**/*.js'],
         tasks: ['exec:rsync']
+    },
+    tdd: {
+        files: [
+            'src/**/*.js',
+            'tests/**/*.js',
+            'Gruntfile.js',
+            'scripts/**/*.js'
+        ],
+        tasks: ['jshint', 'test:unit']
     }
 };
---
+++ tasks/tdd.js
@@ -0,0 +1,3 @@
+module.exports = function(grunt) {
+    grunt.registerTask('tdd', 'runs tests on code changes', 'watch:tdd');
+};
