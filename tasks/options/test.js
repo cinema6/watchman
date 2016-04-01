a9d14ceadcd8d88dfb262bd2beaee3b9415e5c73
Scott Bouloutian <ScottBouloutian@gmail.com>
2016-04-01 12:30:51 -0400

    support simultaneous development

--- tasks/options/test.js
+++ tasks/options/test.js
@@ -1,11 +1,15 @@
+'use strict';
+
+var watchmanUser = process.env.WATCHMAN_USER || process.env.USER || 'anon';
+
 module.exports = {
     unit: { },
     e2e: {
         options: {
             mongoHost: '33.33.33.10',
-            timeStream: 'devTimeStream',
-            watchmanStream: 'devWatchmanStream',
-            cwrxStream: 'devCwrxStream'
+            timeStream: 'devTimeStream-' + watchmanUser,
+            watchmanStream: 'devWatchmanStream-' + watchmanUser,
+            cwrxStream: 'devCwrxStream-' + watchmanUser
         }
     }
 };
