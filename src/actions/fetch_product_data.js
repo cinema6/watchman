cbb951e6ea529959e4eb83d2b094c7da90c16665
Olivia Shanley <olivia.shanley@gmail.com>
2016-06-22 13:06:45 -0400

    updated isEqual to handle deep equivalencies

--- src/actions/fetch_product_data.js
+++ src/actions/fetch_product_data.js
@@ -13,30 +13,43 @@ module.exports = function fetchProductDataFactory(config) {
 	var dataEndpoint = resolveURL(config.cwrx.api.root, config.cwrx.api.productData.endpoint);
     var campEndpoint = resolveURL(config.cwrx.api.root, config.cwrx.api.campaigns.endpoint);
 
-	function isObject(obj) {
-	    return((typeof obj === 'object') && (obj !== null));
-	}
+		function isObject(obj) {
+		    return((typeof obj === 'object') && (obj !== null));
+		}
 
-	function isEqual(object1, object2) {
-
-	    if (typeof object1 !== typeof object2) {
-	        return false;
-	    }
-	    else if ((!isObject(object1))&&(!isObject(object2))) {
-	        return (object1 === object2);
-	    }
-	    else {
-	        if (Object.keys(object1).length!==Object.keys(object2).length) {
-	            return false;
-	        }
-	        for (var prop in object1) {
-	            if (object1[prop] !== object2[prop]) {
-	                return isEqual(object1[prop], object2[prop]);
+		function isEqual(object1, object2) {
+		    if (typeof object1 !== typeof object2) { //type mismatch
+		        return false;
+		    }
+		    else if ((!isObject(object1))&&(!isObject(object2))) {
+	            if (object1 !== object2) { //if not equal
+	                return false;
 	            }
-	        }
-	    	return true;
-	    }
-	}
+		    }
+		    else if ((isObject(object1))&&(isObject(object2))) { //if both objects
+		        if (Object.keys(object1).length!==Object.keys(object2).length) {
+		            return false; //if one is missing properties
+		        }
+		        for (var prop in object1) {
+		            if (((isObject(object1[prop]))&&(isObject(object2[prop])))) {
+	                    for (var subProp in object1[prop]) {
+	                        if (isEqual(object1[prop], object2[prop]) === false) {
+	                            return false;
+	                        }
+	                    }
+		            }
+	                if (typeof object1[prop] !== typeof object2[prop]) { //type mismatch
+	        	        return false;
+	        	    }
+	        	    else if ((!isObject(object1[prop]))&&(!isObject(object2[prop]))) {
+	        	        if (object1[prop] !== object2[prop]) { //if not equal
+	                        return false;
+	                    }
+	        	    }
+		        }
+		    }
+	        return true;
+		}
 
 	return function fetchProductData(event) {
 		var campaign = event.data.campaign;
