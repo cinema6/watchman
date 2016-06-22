16272c4eae5b9535adde0c77edd5c9f9b8e70601
Olivia Shanley <olivia.shanley@gmail.com>
2016-06-22 10:08:19 -0400

    cleaned up logic

--- src/actions/fetch_product_data.js
+++ src/actions/fetch_product_data.js
@@ -7,68 +7,45 @@ var JsonProducer 		= require('rc-kinesis').JsonProducer,
 	q 					= require('q'),
 	log					= logger.getLog();
 
-var objLrg, objSml;
-
 module.exports = function fetchProductDataFactory(config) {
 	var watchmanStream = new JsonProducer(config.kinesis.producer.stream, config.kinesis.producer);
     var request = new CwrxRequest(config.appCreds);
 	var dataEndpoint = resolveURL(config.cwrx.api.root, config.cwrx.api.productData.endpoint);
     var campEndpoint = resolveURL(config.cwrx.api.root, config.cwrx.api.campaigns.endpoint);
 
+	function isObject(obj) {
+	    return((typeof obj === 'object') && (obj !== null));
+	}
+
 	function isEqual(object1, object2) {
-	    if (typeof object1 === 'object') {
-	        if (typeof object2 === 'object') {
-	            if (object1 === null) {
-	                if (object2 === null) {
-	                    return true;
-	                }
-	                else {return false;}
-	            }
 
-	            if (Object.keys(object1).length>=Object.keys(object2).length) {
-	                objLrg = object1;
-	                objSml = object2;
-	            }
-	            else {
-	                objLrg = object2;
-	                objSml = object1;
-	            }
-
-	            for (var prop in objLrg) {
-	                if (typeof objLrg[prop] === 'undefined') {
-	                    if (typeof objSml[prop] !== 'undefined') { //if obj 1 is undef only
-	                        return false;
-	                    }
-	                }
-	                else if (typeof objSml[prop] === 'undefined') { //if obj2 is undef only
-	                    return false;
-	                }
-	                else if (objLrg[prop] !== objSml[prop]) {
-	                     //if different values for same property
-	                        for (var subProp in objLrg[prop]) {
-	                            //if either has subProperties, perform recursion
-	                            if (typeof objLrg[prop][subProp] !== 'undefined') {
-	                                return isEqual(objLrg[prop], objSml[prop]);
-	                            }
-	                            else if (typeof objSml[prop][subProp] !== 'undefined') {
-	                                return isEqual(objLrg[prop], objSml[prop]);
-	                            }
-	                        }
-	                        //otherwise property values aren't equal
-	                        return false;
-	                }
-	            }
-	            return true; //if iterated through all and didn't return false
-	        }
-	        else { //if one is an object but the other isn't
-	            return false;
-	        }
+	    if (typeof object1 !== typeof object2) {
+	        return false;
+	    }
+	    else if ((!isObject(object1))&&(!isObject(object2))) {
+	        return (object1 === object2);
 	    }
 	    else {
-	        if (object1===object2) { //handles non-objects
-	            return true;
+	        var objLrg, objSml;
+
+	        if (Object.keys(object1).length>=Object.keys(object2).length) {
+	            objLrg = object1;
+	            objSml = object2;
 	        }
-	        else {return false;} //handles type mismatch
+	        else {
+	            objLrg = object2;
+	            objSml = object1;
+	        }
+
+	        for (var prop in objLrg) {
+	            if (typeof objLrg[prop] !== typeof objSml[prop]) {
+	                return false;
+	            }
+	            else if (objLrg[prop] !== objSml[prop]) {
+	                return isEqual(objLrg[prop], objSml[prop]);
+	            }
+	        }
+	        return true;
 	    }
 	}
 
