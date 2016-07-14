4e27b1812ab235246a9a89f1a598172387360401
Scott Bouloutian <ScottBouloutian@gmail.com>
2016-07-14 11:12:23 -0400

    update waiter

--- tests/helpers/waiter.js
+++ tests/helpers/waiter.js
@@ -4,13 +4,17 @@ const waiter = {
     // Polls a Promise waiting until it resolves with a truthy value. This function itself returns
     //     a Promise which resolves with the value of the given polled promise.
     waitFor: function(fn, waitTime) {
-        return Promise.resolve(fn()).then(value => {
-            return value || new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    waiter.waitFor(fn, waitTime).then(resolve, reject);
-                }, waitTime);
-            });
-        });
+        const millis = waitTime || 1000;
+        return Promise.resolve(fn()).then(value =>
+            value || waiter.delay(millis).then(() => waiter.waitFor(fn, millis))
+        );
+    },
+
+    // Returns a Promise which resolves once a given amount of time has passed
+    delay: function(millis) {
+        return new Promise(resolve => setTimeout(() => {
+            resolve();
+        }, millis));
     }
 };
 module.exports = waiter;
