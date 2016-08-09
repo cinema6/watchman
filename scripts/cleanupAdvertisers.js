cd21321f3a2094e17ae4e42022d28a36b46e29b7
Howard Engelhart <howard.engelhart@outlook.com>
2016-08-09 17:02:02 -0400

    updated cleanupAdvertisers script

--- scripts/cleanupAdvertisers.js
+++ scripts/cleanupAdvertisers.js
@@ -1,10 +1,9 @@
 var request = require('request'),
     url     = require('url'),
     q       = require('q'),
-    BeeswaxHelper = require('../tests/helpers/BeeswaxHelper'),
+    path    = require('path'),
+    BeeswaxClient = require('beeswax-client'),
     task    = {};
-    //authUrl   = server + '/api/auth/login',
-    //queryUrl  = server + '/api/analytics/campaigns';
 
 function initTask(task) {
     var creds = require(process.env.HOME + '/.c6api.json'), u;
@@ -33,7 +32,11 @@ function initTask(task) {
 
     task.authUrl = u.protocol + '//' + u.host + '/api/auth/login';
     task.rqsUrl  = u.href;
-    task.beeswax = new BeeswaxHelper();
+   
+    var bwEnv = u.host.match(/staging/) ? 'test' : 'prod';
+    var bwCreds = require(path.join(process.env.HOME,'.bw.json'))[bwEnv];
+    
+    task.beeswax = new BeeswaxClient({ apiRoot : bwCreds.hostname, creds : bwCreds });
 
     return task;
 }
@@ -52,12 +55,9 @@ function authenticate(task) {
    
     request.post(loginOpts, function(error, response, body) {
         if (error) {
-//            console.log('Login error: ', error);
             return deferred.reject(error);
         }
         else if (response.statusCode !== 200) {
-//            console.log('Login failure: ', response.statusCode);
-//            console.log(body);
             return deferred.reject(body);
         }
         
@@ -77,12 +77,9 @@ function getAdvertisers(task) {
    
     request.get(opts, function(error, response, body) {
         if (error) {
-//            console.log(' Error: ', error);
             return deferred.reject(error);
         }
         else if (response.statusCode !== 200) {
-//            console.log(' Failed: ', response.statusCode);
-//            console.log(body);
             return deferred.reject(body);
         }
         task.advertisers = body;  
@@ -93,17 +90,30 @@ function getAdvertisers(task) {
 }
 
 function getBeeswaxStuff(task){
-    return q.all(task.advertisers.filter(function(adv){
+    var beeswaxAdvertisers = task.advertisers.filter(function(adv){
         return ((adv.externalIds && adv.externalIds.beeswax) || 
-            (adv.beeswaxIds && adv.beeswaxIds.advertiser));
-    }).map(function(adv){
+        (adv.beeswaxIds && adv.beeswaxIds.advertiser));
+    });
+
+    //var count = 0;
+
+    function lookupAdvertiser(){
+        var adv = beeswaxAdvertisers.shift();
+        if (!adv) {
+            return task;
+        }
+
+        //if (count++ === 25) {
+        //    return task;
+        //}
+        
         var advertiserId = ((adv.externalIds && adv.externalIds.beeswax) || 
             (adv.beeswaxIds && adv.beeswaxIds.advertiser));
-//        console.log('SEARCH ADVERTISER ID:',advertiserId);
+        console.log('Lookup beeswax data for ', adv.name, ' [', advertiserId, ']');
         return q.all([
-            task.beeswax.api.advertisers.find( advertiserId ),
-            task.beeswax.api.campaigns.queryAll({ advertiser_id : advertiserId }),
-            task.beeswax.api.creatives.queryAll({ advertiser_id : advertiserId })
+            task.beeswax.advertisers.find( advertiserId ),
+            task.beeswax.campaigns.queryAll({ advertiser_id : advertiserId }),
+            task.beeswax.creatives.queryAll({ advertiser_id : advertiserId })
         ])
         .spread(function(bwad,bwcamp,bwcreat){
             adv.beeswax = {
@@ -112,26 +122,82 @@ function getBeeswaxStuff(task){
                 campaign : bwcamp.payload[0] ? true : false,
                 creative : bwcreat.payload[0] ? true : false
             };
+
+            return lookupAdvertiser();
         });
-    }))
-    .then(function(ads){
-//        console.log('GIT ADS:',ads);
-        task.advertisers = ads.filter(function(ad){
-            return ad.beeswax && ad.beeswax.advertiser &&
-                ( !ad.beeswax.campaign && !ad.beeswax.creative);
-        });
-        return task;
+    }
+
+    return lookupAdvertiser();
+}
+
+function cleanseAdvertiser(task){
+    var advertisers = task.advertisers.filter(function(ad){
+        return ad.beeswax && ad.beeswax.advertiser &&
+            ( !ad.beeswax.campaign && !ad.beeswax.creative);
+    }).sort(function(a,b){
+        return parseInt(a.beeswax.advertiser_id,10) > parseInt(b.beeswax.advertiser_id,10) ? 1 : -1;
     });
+    console.log('Total Advertisers: ', task.advertisers.length,
+        ', Empty: ', advertisers.length);
+
+    function updateReelcontent(adv) {
+        var opts = {
+                url: task.rqsUrl + 'api/account/advertisers/' + adv.id,
+                rejectUnauthorized : false,
+                jar : true,
+                json : {
+                    beeswaxIds : null,
+                    externalIds : null
+                }
+            }, deferred = q.defer();
+      
+        console.log('   Reelcontent');
+        request.put(opts, function(error, response, body) {
+            if (error) {
+                return deferred.reject(error);
+            }
+            else if (response.statusCode !== 200) {
+                return deferred.reject(body);
+            }
+            task.advertisers = body;  
+            return deferred.resolve(adv);
+        });
+        
+        return deferred.promise;
+    }
+
+    function updateBeeswax(adv){
+        console.log('   Beeswax');
+        return task.beeswax.advertisers.delete(adv.beeswax.advertiser_id);
+    }
+
+    function cleanAdvertiser(){
+        var adv = advertisers.shift();
+        if (!adv) {
+            return task;
+        }
+        var advertiserId = adv.beeswax.advertiser_id;
+        console.log('Remove beeswax data for ', adv.name, ' [', advertiserId, '] [',
+                adv.id, ']');
+
+//        return cleanAdvertiser();
+        return updateReelcontent(adv)
+            .then(updateBeeswax)
+            .then(cleanAdvertiser);
+    }
+
+    return cleanAdvertiser();
 }
 
 q(initTask(task))
 .then(authenticate)
 .then(getAdvertisers)
-.then(getBeeswaxStuff);
-//.then(function(task){
-////    console.log(task.advertisers);
-//})
-//.catch(function(e){
-////    console.log(e);
-//});
+.then(getBeeswaxStuff)
+.then(cleanseAdvertiser)
+.then(function(){
+    console.log('Done');
+})
+.catch(function(e){
+    console.log(e.stack);
+});
 
